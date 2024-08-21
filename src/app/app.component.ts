908830fcfc6c7feb5edb4b4b7aeb3b108412bc3d
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-08-21 23:55:58 +0300

    created item-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';//додано - імпортовано
+import { NgForOf } from '@angular/common';//додано - імпортовано
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GreetingComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '20.08.24_ngFor';
+  quote = 'Welcome to the Angular application!';
+  name = 'MyName';
 }
--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-greeting',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './greeting.component.css'
 })
 export class GreetingComponent {
-
+  @Input() quote:string='';
+  @Input() name:string='';
 }
